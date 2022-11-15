efd750abc3bd0a1e2cb8646037b035b25e0f8c9e
Aditia Darma Nasution <ad.nasution_pro@alphaphilindo.com>
2022-11-15 12:55:56 +0700

    update for production

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ BASE_URL = "http://127.0.0.1:5000/"
 @app.route('/')
 def index():
     dr = {'BASE_URL': BASE_URL}
-    return render_template('home.html')
+    return render_template('home.html', dRes=dr)
 
+@app.route('/data-training')
+def dataTraining():
+    dr = {'BASE_URL': BASE_URL}
+    return render_template('data-training.html', dRes=dr)
+
+@app.route('/proses-data-training', methods=('GET', 'POST'))
+def prosesDataTraining():
+    dr = {"status" : "success"}
+    return jsonify(dr)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
